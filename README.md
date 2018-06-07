d37b501f022b55da9963437dcfce8685abe07388
Bill Zimmerlin <wsz2800@gmail.com>
2018-06-07 14:18:04 +0000

    added web view

---
+++ README.md
@@ -0,0 +1,6 @@
+zjn700:~/workspace/feathers/basic-feathers (master) $ http-server public/
+Starting up http-server, serving public/
+Available on:
+  http://127.0.0.1:8080
+  http://172.17.0.143:8080
+Hit CTRL-C to stop the server
--- app.js
+++ app.js
@@ -1,30 +1,98 @@
 const feathers = require('@feathersjs/feathers');
-const app = feathers();
 
-console.log('here in app.js')
-
-// Register a simple todo service that returns the name and some text
-app.use('todos', {
-  async get(name) {
-    // Return an object in the form of { name, text }
-    return {
-      name,
-      text: `You have to do ${name}`
-    };
+class Messages {
+  constructor() {
+    this.messages = [];
+    this.currentId = 0;
   }
-});
 
+  async find(params) {
+    // Return the list of all messages
+    return this.messages;
+  }
 
+  async get(id, params) {
+    // Find the message by id
+    const message = this.messages.find(message => message.id === parseInt(id, 10));
 
-// A function that gets and logs a todo from the service
-async function getTodo(name) {
-  // Get the service we registered above
-  const service = app.service('todos');
-  // Call the `get` method with a name
-  const todo = await service.get(name);
+    // Throw an error if it wasn't found
+    if(!message) {
+      throw new Error(`Message with id ${id} not found`);
+    }
 
-  // Log the todo we got back
-  console.log(todo);
+    // Otherwise return the message
+    return message;
+  }
+
+  async create(data, params) {
+    // Create a new object with the original data and an id
+    // taken from the incrementing `currentId` counter
+    const message = Object.assign({
+      id: ++this.currentId
+    }, data);
+
+    this.messages.push(message);
+
+    return message;
+  }
+
+  async patch(id, data, params) {
+    // Get the existing message. Will throw an error if not found
+    const message = await this.get(id);
+
+    // Merge the existing message with the new data
+    // and return the result
+    return Object.assign(message, data);
+  }
+
+  async remove(id, params) {
+    // Get the message by id (will throw an error if not found)
+    const message = await this.get(id);
+    // Find the index of the message in our message array
+    const index = this.messages.indexOf(message);
+
+    // Remove the found message from our array
+    this.messages.splice(index, 1);
+
+    // Return the removed message
+    return message;
+  }
 }
 
-getTodo('dishes');
+const app = feathers();
+
+// Initialize the messages service by creating
+// a new instance of our class
+app.use('messages', new Messages());
+
+
+
+////////////////
+
+
+async function processMessages() {
+  app.service('messages').on('created', message => {
+    console.log('Created a new message', message);
+  });
+
+  app.service('messages').on('removed', message => {
+    console.log('Deleted message', message);
+  });
+
+  await app.service('messages').create({
+    text: 'First message'
+  });
+
+  const lastMessage = await app.service('messages').create({
+    text: 'Second message'
+  });
+
+  // Remove the message we just created
+  await app.service('messages').remove(lastMessage.id);
+
+  const messageList = await app.service('messages').find();
+
+  console.log('Available messages', messageList);
+}
+
+processMessages();
